d2769ee67437a9b81c6521e5cb49d0fbc4f073ea
Donato Montrone <114031046+donatomontrone@users.noreply.github.com>
2026-09-08 15:53:59 +0200

    feat: load shared UI interaction helpers in panel

--- src/panel.tsx
+++ src/panel.tsx
@@ -3,6 +3,7 @@ import { createRoot, type Root } from "react-dom/client";
 import App from "./App";
 import type { Hass } from "./types";
 import "./segmented-gestures";
+import "./ui-interactions";
 import baseStyles from "./styles.css?inline";
 import themeStyles from "./theme.css?inline";
 import homeStyles from "./home-view.css?inline";
