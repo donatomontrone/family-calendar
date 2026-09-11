d27b35d0ba7301983c18a84e589e6a990edbbd85
Donato Montrone <114031046+donatomontrone@users.noreply.github.com>
2026-09-11 12:40:45 +0200

    style: load improved Casa accessory layout

--- src/SharedHeader.tsx
+++ src/SharedHeader.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./ui-interactions";
+import "./home-devices-v6.css";
 import type { Hass } from "./types";
 import type { Language } from "./i18n";
 
