1f2ed9c1b4e7bc88fcb46dc7eb8726b435e4c8c4
Donato Montrone <114031046+donatomontrone@users.noreply.github.com>
2026-09-13 08:21:27 +0200

    include shared modal contract in HA bundle

--- src/panel.tsx
+++ src/panel.tsx
@@ -29,6 +29,7 @@ import mobilePhoneFixesV9Styles from "./mobile-phone-fixes-v9.css?inline";
 import homePhoneRoomPolishV10Styles from "./home-phone-room-polish-v10.css?inline";
 import homeMobileUtilitiesV20Styles from "./home-mobile-utilities-v20.css?inline";
 import homeMobileFeaturesV21Styles from "./home-mobile-features-v21.css?inline";
+import sharedModalContractV22Styles from "./shared-modal-contract-v22.css?inline";
 
 const STYLE_ID = "family-calendar-panel-styles";
 
@@ -36,7 +37,7 @@ function ensureStyles() {
   if (document.getElementById(STYLE_ID)) return;
   const style = document.createElement("style");
   style.id = STYLE_ID;
-  style.textContent = `${baseStyles}\n${themeStyles}\n${homeStyles}\n${appleHomeStyles}\n${stabilityStyles}\n${reelHomeStyles}\n${climateStyles}\n${homeFeatureStyles}\n${readabilityStyles}\n${segmentedStyles}\n${designSystemStyles}\n${designTuningStyles}\n${calendarSmartHomePolishStyles}\n${smartHomeDeviceCardStyles}\n${controlLanguageStyles}\n${appleGuidelinesStyles}\n${calendarV3Styles}\n${homeRoomSharedAccentStyles}\n${homeMobileV8Styles}\n${mobilePhoneFixesV9Styles}\n${homePhoneRoomPolishV10Styles}\n${homeMobileUtilitiesV20Styles}\n${homeMobileFeaturesV21Styles}`;
+  style.textContent = `${baseStyles}\n${themeStyles}\n${homeStyles}\n${appleHomeStyles}\n${stabilityStyles}\n${reelHomeStyles}\n${climateStyles}\n${homeFeatureStyles}\n${readabilityStyles}\n${segmentedStyles}\n${designSystemStyles}\n${designTuningStyles}\n${calendarSmartHomePolishStyles}\n${smartHomeDeviceCardStyles}\n${controlLanguageStyles}\n${appleGuidelinesStyles}\n${calendarV3Styles}\n${homeRoomSharedAccentStyles}\n${homeMobileV8Styles}\n${mobilePhoneFixesV9Styles}\n${homePhoneRoomPolishV10Styles}\n${homeMobileUtilitiesV20Styles}\n${homeMobileFeaturesV21Styles}\n${sharedModalContractV22Styles}`;
   document.head.appendChild(style);
 }
 
