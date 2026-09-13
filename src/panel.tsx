d517d1b951ccd3a893b385beb16a7f1a4dea22d6
Donato Montrone <114031046+donatomontrone@users.noreply.github.com>
2026-09-13 08:06:20 +0200

    include mobile home feature polish in HA bundle

--- src/panel.tsx
+++ src/panel.tsx
@@ -28,6 +28,7 @@ import homeMobileV8Styles from "./home-mobile-v8.css?inline";
 import mobilePhoneFixesV9Styles from "./mobile-phone-fixes-v9.css?inline";
 import homePhoneRoomPolishV10Styles from "./home-phone-room-polish-v10.css?inline";
 import homeMobileUtilitiesV20Styles from "./home-mobile-utilities-v20.css?inline";
+import homeMobileFeaturesV21Styles from "./home-mobile-features-v21.css?inline";
 
 const STYLE_ID = "family-calendar-panel-styles";
 
@@ -35,7 +36,7 @@ function ensureStyles() {
   if (document.getElementById(STYLE_ID)) return;
   const style = document.createElement("style");
   style.id = STYLE_ID;
-  style.textContent = `${baseStyles}\n${themeStyles}\n${homeStyles}\n${appleHomeStyles}\n${stabilityStyles}\n${reelHomeStyles}\n${climateStyles}\n${homeFeatureStyles}\n${readabilityStyles}\n${segmentedStyles}\n${designSystemStyles}\n${designTuningStyles}\n${calendarSmartHomePolishStyles}\n${smartHomeDeviceCardStyles}\n${controlLanguageStyles}\n${appleGuidelinesStyles}\n${calendarV3Styles}\n${homeRoomSharedAccentStyles}\n${homeMobileV8Styles}\n${mobilePhoneFixesV9Styles}\n${homePhoneRoomPolishV10Styles}\n${homeMobileUtilitiesV20Styles}`;
+  style.textContent = `${baseStyles}\n${themeStyles}\n${homeStyles}\n${appleHomeStyles}\n${stabilityStyles}\n${reelHomeStyles}\n${climateStyles}\n${homeFeatureStyles}\n${readabilityStyles}\n${segmentedStyles}\n${designSystemStyles}\n${designTuningStyles}\n${calendarSmartHomePolishStyles}\n${smartHomeDeviceCardStyles}\n${controlLanguageStyles}\n${appleGuidelinesStyles}\n${calendarV3Styles}\n${homeRoomSharedAccentStyles}\n${homeMobileV8Styles}\n${mobilePhoneFixesV9Styles}\n${homePhoneRoomPolishV10Styles}\n${homeMobileUtilitiesV20Styles}\n${homeMobileFeaturesV21Styles}`;
   document.head.appendChild(style);
 }
 
