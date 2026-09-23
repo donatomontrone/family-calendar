b6e9d7b144ab925645aa84e39732815e25e1c9e2
Donato Montrone <114031046+donatomontrone@users.noreply.github.com>
2026-09-23 10:25:06 +0200

    fix: remove Calendar tablet experiment from ui-interactions.ts

--- src/ui-interactions.ts
+++ src/ui-interactions.ts
@@ -24,7 +24,6 @@ import homeMobilePowerV35Styles from "./home-mobile-power-v35.css?inline";
 import phonePortraitControlsV51Styles from "./phone-portrait-controls-v51.css?inline";
 import alarmLandscapeV52Styles from "./alarm-landscape-v52.css?inline";
 import largeScreenV60Styles from "./large-screen-v60.css?inline";
-import calendarTabletV62Styles from "./calendar-tablet-v62.css?inline";
 
 type RoomDragState = {
   strip: HTMLElement;
@@ -51,7 +50,7 @@ type UiWindow = Window & {
 
 const uiWindow = window as UiWindow;
 const FINAL_STYLE_ID = "family-calendar-v4-styles";
-const finalStyles = `${calendarV4Styles}\n${calendarV5Styles}\n${calendarV6Styles}\n${calendarV7Styles}\n${calendarV8Styles}\n${calendarV9Styles}\n${calendarV10Styles}\n${calendarV11Styles}\n${calendarV12Styles}\n${calendarV13Styles}\n${homeV1Styles}\n${homeV2Styles}\n${homeV3Styles}\n${homeLiquidGlassStyles}\n${homeRoomRefinementStyles}\n${homeSharedUiV12Styles}\n${sharedExactV13Styles}\n${sharedExactV14Styles}\n${sharedExactV15Styles}\n${homeDesktopRoomInfoV24Styles}\n${desktopRoomWorkspaceV26Styles}\n${desktopRoomMobileParityV30Styles}\n${homeMobilePowerV35Styles}\n${phonePortraitControlsV51Styles}\n${alarmLandscapeV52Styles}\n${largeScreenV60Styles}\n${calendarTabletV62Styles}`;
+const finalStyles = `${calendarV4Styles}\n${calendarV5Styles}\n${calendarV6Styles}\n${calendarV7Styles}\n${calendarV8Styles}\n${calendarV9Styles}\n${calendarV10Styles}\n${calendarV11Styles}\n${calendarV12Styles}\n${calendarV13Styles}\n${homeV1Styles}\n${homeV2Styles}\n${homeV3Styles}\n${homeLiquidGlassStyles}\n${homeRoomRefinementStyles}\n${homeSharedUiV12Styles}\n${sharedExactV13Styles}\n${sharedExactV14Styles}\n${sharedExactV15Styles}\n${homeDesktopRoomInfoV24Styles}\n${desktopRoomWorkspaceV26Styles}\n${desktopRoomMobileParityV30Styles}\n${homeMobilePowerV35Styles}\n${phonePortraitControlsV51Styles}\n${alarmLandscapeV52Styles}\n${largeScreenV60Styles}`;
 const CALENDAR_TONES = ["mint", "blue", "amber", "violet"];
 
 function ensureFinalStyles() {
